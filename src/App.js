8926b256401751e6c6e9f574b729ea48439ca288
Anuhya Sai Nudurupati <anuhya.sai28@gmail.com>
2018-12-22 11:51:33 -0700

    Try 11

--- src/App.js
+++ src/App.js
@@ -42,26 +42,15 @@ class App extends React.Component{
               <NavItem>
                 <NavLink href={CONSTANTS.TASK_LIST}>Task List</NavLink>
               </NavItem>
-              {/* <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>Option 1</DropdownItem>
-                  <DropdownItem>Option 2</DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>Reset</DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown> */}
             </Nav>
           </Collapse>
         </Navbar>
         <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Switch>
-        <Route exact={true} path={"/" + routes.SERVICE_FORM} component={ServiceRequestForm} />
+        <Route exact={true} path={process.env.PUBLIC_URL + "/" + routes.SERVICE_FORM} component={ServiceRequestForm} />
         {/* <Route exact={true} path={routes.LOGIN} component={LoginPage} />
         <Route exact={true} path={routes.REGISTER} component={RegisterPage} /> */}
-        <Route exact={true} path={"/" + routes.TASK_LIST} component={TaskList} />
+        <Route exact={true} path={process.env.PUBLIC_URL + "/" + routes.TASK_LIST} component={TaskList} />
         <Route component={() => (<div>404 Not found </div>)} />
         </Switch>
         </BrowserRouter>
