622503d956cdf6d45fcbf7e28e0ff02ceed0b240
Anuhya Sai Nudurupati <anuhya.sai28@gmail.com>
2018-12-22 10:58:03 -0700

    Updates

---
+++ precache-manifest.62a6881b7aad90dcc69c16f575f34545.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec21bb58fccb95865a18",
+    "url": "/insure-clientSide/static/js/runtime~main.ec21bb58.js"
+  },
+  {
+    "revision": "b04177e4cb53e4a52663",
+    "url": "/insure-clientSide/static/js/main.b04177e4.chunk.js"
+  },
+  {
+    "revision": "4c40954633a796531178",
+    "url": "/insure-clientSide/static/js/1.4c409546.chunk.js"
+  },
+  {
+    "revision": "b04177e4cb53e4a52663",
+    "url": "/insure-clientSide/static/css/main.e85987e1.chunk.css"
+  },
+  {
+    "revision": "4c40954633a796531178",
+    "url": "/insure-clientSide/static/css/1.aa4e15f2.chunk.css"
+  },
+  {
+    "revision": "1c1e6120b393af275b5ddfb618eeebc6",
+    "url": "/insure-clientSide/index.html"
+  }
+];
